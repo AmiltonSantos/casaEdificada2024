0db5ef1a198cb52ea17073c4547fccdad6d25c12
Amilton Santos <amilton@artnew.com.br>
2024-06-03 17:47:11 -0300

    :fire: ajusta video de fundo casa edificada

--- components/Content.jsx
+++ components/Content.jsx
@@ -2,31 +2,18 @@
 
 const Content = () => {
   return (
-    
-    
-      <div className="contentContainer">
-      <div className="relative">
+    <div className="static">
+      <video
+        src="/casaedificada.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover"
+      />
 
-        <video 
-          src="/casaedificada.mp4" 
-          autoPlay 
-          loop 
-          muted
-          className="absolute w-full h-full object-cover"
-        />
-
-        <div className="relative z-10">
-          {/* Conteúdo da página por cima do vídeo */}
-      </div>
-
-      </div>
-      <img className="coverImage" src="/the-hunch-mobile.png" alt="" />
-      <div className="dummyText">
-        <p>
-          <span>EM 2024</span>
-        </p>
-        <h1>CASA EDIFICADA</h1>
-        <p>Venha e participe!</p>
+      <div className="relative z-10">
+        {/* Conteúdo da página por cima do vídeo */}
       </div>
     </div>
   );
