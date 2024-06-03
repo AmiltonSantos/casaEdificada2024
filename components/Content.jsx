8a34064e83579374890c0f38f8cd472b316dd4c4
Amilton Santos <a1000ton1000ton@gmail.com>
2024-06-03 00:02:10 -0300

    :art: cria video plano de fundo

--- components/Content.jsx
+++ components/Content.jsx
@@ -3,7 +3,23 @@
 const Content = () => {
   return (
     
-    <div className="contentContainer">
+    
+      <div className="contentContainer">
+      <div className="relative">
+
+        <video 
+          src="/casaedificada.mp4" 
+          autoPlay 
+          loop 
+          muted
+          className="absolute w-full h-full object-cover"
+        />
+
+        <div className="relative z-10">
+          {/* Conteúdo da página por cima do vídeo */}
+      </div>
+
+      </div>
       <img className="coverImage" src="/the-hunch-mobile.png" alt="" />
       <div className="dummyText">
         <p>
